e9856a23a3f92f61f8efe66834d6ecaa45bd2285
Ricky Tanudjaja <ricky.tanudjaja@shopee.com>
2023-06-17 11:31:19 +0700

    styling for NewThread

--- src/Domains/threads/entities/_test/NewThread.test.js
+++ src/Domains/threads/entities/_test/NewThread.test.js
@@ -4,29 +4,33 @@ describe('NewThread entities', () => {
   it('should throw error when payload not contain needed property', () => {
     // Arrange
     const payload = {
-      title: "sebuah thread",
+      title: 'sebuah thread',
     };
 
     // Action & Assert
-    expect(() => new NewThread(payload)).toThrowError('NEW_THREAD.NOT_CONTAIN_NEEDED_PROPERTY');
+    expect(() => new NewThread(payload)).toThrowError(
+      'NEW_THREAD.NOT_CONTAIN_NEEDED_PROPERTY'
+    );
   });
 
   it('should throw error when payload not meet data type specification', () => {
     // Arrange
     const payload = {
-      title: "sebuah thread",
-      body: 1234
+      title: 'sebuah thread',
+      body: 1234,
     };
 
     // Action & Assert
-    expect(() => new NewThread(payload)).toThrowError('NEW_THREAD.NOT_MEET_DATA_TYPE_SPECIFICATION');
+    expect(() => new NewThread(payload)).toThrowError(
+      'NEW_THREAD.NOT_MEET_DATA_TYPE_SPECIFICATION'
+    );
   });
 
   it('should create NewThread entities correctly', () => {
     // Arrange
     const payload = {
-      title: "sebuah thread",
-      body: "isi body yang lengkap"
+      title: 'sebuah thread',
+      body: 'isi body yang lengkap',
     };
 
     // Action
