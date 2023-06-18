dec28d8ba18da43904f94f1270bafbca7b636266
Ricky Tanudjaja <ricky.tanudjaja@shopee.com>
2023-06-18 19:51:39 +0700

    add thread repository postgres

---
+++ tests/ThreadsTableTestHelper.js
@@ -0,0 +1,31 @@
+/* istanbul ignore file */
+const pool = require('../src/Infrastructures/database/postgres/pool');
+
+const ThreadsTableTestHelper = {
+  async addThread({
+    id = 'thread-123', title = 'sebuah thread', body = 'isi body yang lengkap', owner = 'user-456', created_at = '2023-06-16T01:02:03.456Z', updated_at = '2023-06-16T01:02:03.456Z',
+  }) {
+    const query = {
+      text: 'INSERT INTO threads VALUES($1, $2, $3, $4, $5, $6)',
+      values: [id, title, body, owner, created_at, updated_at],
+    };
+
+    await pool.query(query);
+  },
+
+  async findThreadsById(id) {
+    const query = {
+      text: 'SELECT * FROM threads WHERE id = $1',
+      values: [id],
+    };
+
+    const result = await pool.query(query);
+    return result.rows;
+  },
+
+  async cleanTable() {
+    await pool.query('DELETE FROM threads WHERE 1=1');
+  },
+};
+
+module.exports = ThreadsTableTestHelper;
