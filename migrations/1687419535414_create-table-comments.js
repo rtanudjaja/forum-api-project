2c7d141f5c58545eb82ae2370c775e21df5ec465
Ricky Tanudjaja <ricky.tanudjaja@shopee.com>
2023-06-22 14:45:52 +0700

    finish migration comment table

---
+++ migrations/1687419535414_create-table-comments.js
@@ -0,0 +1,33 @@
+/* eslint-disable camelcase */
+exports.up = (pgm) => {
+  pgm.createTable('comments', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    content: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    owner: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    created_at: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    updated_at: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    is_delete: {
+      type: 'BOOLEAN',
+      notNull: true,
+    }
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('comments');
+};
